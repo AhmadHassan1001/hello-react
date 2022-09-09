b5306eb797f2df561287ff03285d6fb024ce03a9
MidoHassan <mi0102032690@gmail.com>
2022-09-09 09:45:13 +0200

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is ahead of 'origin/master' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -21,16 +21,16 @@ function App() {
   console.log("Parent");
   useEffect(()=>{
     if(data["isloading"]){
-      fetch('http://localhost:4000/homepage')
-      //fetch('https://api.npoint.io/2378fcd8a9ad7ca0cde1')
+      //fetch('http://localhost:4000/homepage')
+      fetch('https://api.npoint.io/764e024c93bc594705f2')
     .then((response)=>{
       return response.json();
     })
     .then((json)=>{
       console.log("json1");
       console.log(json);
-      ///let json2={"data":json["homepage"],"isloading":false,"error":""};
-      let json2={"data":json,"isloading":false,"error":""};
+      let json2={"data":json["homepage"],"isloading":false,"error":""};
+      ///let json2={"data":json,"isloading":false,"error":""};
       setData(json2);
       console.log(json2);
       
