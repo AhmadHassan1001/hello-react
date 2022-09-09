1cb712d6290a1825b046c5f81b7346eb000d239c
MidoHassan <mi0102032690@gmail.com>
2022-09-09 11:12:55 +0200

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   public/styles.css
    	modified:   src/App.css
    	modified:   src/components/CoursesPanel.js

--- src/components/CoursesPanel.js
+++ src/components/CoursesPanel.js
@@ -1,16 +1,67 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import CourseCard from './CourseCard'
 import { DataContext } from '../contexts/dataContext';
 import { Link } from 'react-router-dom';
+import {Carousel} from 'react-bootstrap';
+
 function CoursesPanel() {
   let ListCards=[];
   let data=useContext(DataContext);
   console.log('childContext');
+  const [current,setCurrent]=useState(0);
+  let ListCarousel=[];
+
+  const [windowSize, setWindowSize] = useState(getWindowSize());
+  function getWindowSize() {
+    const {innerWidth, innerHeight} = window;
+    return {innerWidth, innerHeight};
+  }
+  useEffect(() => {
+    function handleWindowResize() {
+      setWindowSize(getWindowSize());
+    }
+
+    window.addEventListener('resize', handleWindowResize);
+
+    return () => {
+      window.removeEventListener('resize', handleWindowResize);
+    };
+  }, []);
+function get_num(){
+  if(windowSize.innerWidth<700){
+    return 1;
+  }
+  if(windowSize.innerWidth<1000){
+    return 2;
+  }
+  if(windowSize.innerWidth<1800){
+    return 4;
+  }
+  return 5;
+}
+
+  const [num_courses_per_panel,setNum]=useState(get_num());
   
+  useEffect(()=>{
+      setNum(get_num());
+  },[windowSize])
+
   
+  console.log("windowSize.innerWidth");
+  console.log(windowSize.innerWidth);
+  console.log(current);
   for(let i in data["data"][0]["items"]){
     let course=data["data"][0]["items"][i];
     ListCards.push(<Link to={`/courses/${data["data"][0]["items"][i]["id"]}` } style={{ textDecoration: 'none',color:"black" }}><CourseCard key={i} title={course["title"]} src={course["cover"]} instructor={course["visible_instructors"]} rate={course["rating"].toFixed(1)} price={course["price"]} students={course["num_subscribers"]} /></Link>);
+    if(i%num_courses_per_panel==num_courses_per_panel-1){
+      ListCarousel.push(<Carousel.Item><div className='courses-cards-panel'>{ListCards}</div></Carousel.Item>);
+      ListCards=[];
+    }
+  }
+  if(ListCards.length>0){
+    ListCarousel.push(<Carousel.Item><div className='courses-cards-panel'>{ListCards}</div></Carousel.Item>);
+    ListCards=[];
+    
   }
   return (
     <div className='course-panel'>
@@ -19,9 +70,12 @@ function CoursesPanel() {
             {data["data"][0]["description"]}
         </p>
         <button className="explore-btn nav-btn">Explore python</button>
-        <div className='courses-cards-panel'>
-            {ListCards}
-        </div>
+        
+            <Carousel
+            >
+            {ListCarousel}
+            
+            </Carousel>
     </div>
   )
 }
